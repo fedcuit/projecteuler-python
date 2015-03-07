6a66ad083c3aa333efa9a37bbf5026625d262df7
Erdong Feng <edfeng@thoughtworks.com>
2015-03-07 16:46:33 +0800

    more concise way to generator fibonacci.

--- problems/problem2.py
+++ problems/problem2.py
@@ -1,23 +1,14 @@
-import sys
 import itertools
 
 __author__ = 'edfeng'
 
 
 def fibonacci_seq():
-    the_one_before_previous = 1
-    previous = 2
-    for i in xrange(1, sys.maxint):
-        if i == 1:
-            yield 1
-        if i == 2:
-            yield 2
-
-        if i > 2:
-            new_value = previous + the_one_before_previous
-            the_one_before_previous = previous
-            previous = new_value
-            yield new_value
+    x = 1
+    y = 2
+    while True:
+        yield x
+        x, y = y, y + x
 
 
 def sum_of_fibs():
--- tests/test_sum_of_even_value_in_fibonacci.py
+++ tests/test_sum_of_even_value_in_fibonacci.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-import itertools
 
 from problems import problem2
 
