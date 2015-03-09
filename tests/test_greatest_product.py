05287b3cee090f83e833e74dddb07988693964fb
Erdong Feng <edfeng@thoughtworks.com>
2015-03-09 14:10:19 +0800

    solve problem11.(string.splitlines() return a list of lines based on line break; if seq return true when col has something, return false when seq is empty, seq can be list, tuple, string)

--- tests/test_greatest_product.py
+++ tests/test_greatest_product.py
@@ -1,10 +1,16 @@
 from unittest import TestCase
 from problems import problem8
+from problems import problem11
 
 __author__ = 'edfeng'
 
 
 class TestProblem8(TestCase):
-    def test_greatest_produce(self):
+    def test_greatest_product(self):
         self.assertEqual(5832, problem8.greatest_product(4))
         self.assertEqual(23514624000, problem8.greatest_product(13))
+
+
+class TestProblem11(TestCase):
+    def test_greatest_product(self):
+        self.assertEqual(70600674, problem11.greatest_product())
