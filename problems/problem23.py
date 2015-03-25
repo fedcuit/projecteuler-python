81b6486e66725eef50f5966f34e49eb915951085
Erdong Feng <edfeng@thoughtworks.com>
2015-03-25 20:15:39 +0800

    more work on performance, replace map based bitmap with list because list is faster on accesing by index.

--- problems/problem23.py
+++ problems/problem23.py
@@ -1,4 +1,3 @@
-import collections
 import itertools
 
 from gevent.pool import Pool
@@ -11,14 +10,14 @@ __author__ = 'edfeng'
 
 
 def is_abundant_number(n):
-    return sum(mathutil.proper_divisors(n)) > n
+    return False if n == 0 else sum(mathutil.proper_divisors(n)) > n
 
 
-abundant_map = collections.OrderedDict({i: is_abundant_number(i) for i in xrange(1, 28123 + 1)})
+abundant_bitmap = [is_abundant_number(i) for i in xrange(28123 + 1)]
 
 
 def not_abundantify_number(n):
-    match = next((i for i in range(1, n) if abundant_map[i] and abundant_map[n - i]), None)
+    match = next((i for i in range(1, n) if abundant_bitmap[i] and abundant_bitmap[n - i]), None)
     return True if match is None else False
 
 
